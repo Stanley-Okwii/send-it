d2736e5b2ecb91e26b821555e728131f3f9ba4f6
Stanley-Okwii <stanleeparker12@gmail.com>
2018-11-14 08:20:36 +0300

    Remove duplicate code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,8 @@
-showPassword = () => {
-    // show = document.getElementsByClassName("fa-eye")[0];
-    // hide = document.getElementsByClassName("fa-eye-slash")[0];
-    // passwordParent = document.getElementById("password-visible");
+showPassword = () => {;
     passwordField = document.getElementById("loginPassword");
 
     if (passwordField.type === 'password') {
         passwordField.type = 'text';
-        // console.log(passwordParent);
       } else {
         passwordField.type = 'password';
       }
